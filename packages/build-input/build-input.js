975d5a58c0d463c64fe3e405fcd041c27c48c3c3
ajaxjs <afenghy@qq.com>
2024-06-11 11:08:34 +0800

    修复插槽BUG

--- packages/build-input/build-input.js
+++ packages/build-input/build-input.js
@@ -23,12 +23,10 @@ export default defineComponent({
             const { label, noPlaceholder } = props;
             const inputType = attrs.type || 'input';
             const inputAttrs = { label, ...attrs, 'onUpdate:modelValue': onUpdate };
-            // 输入框
-            let inputItem = h(getInputItem(inputType), inputAttrs, inputSlots);
-            // 输入框前后插槽
-            let {beforeInput, afterInput} = inputSlots;
             // 输入组件插槽
             inputSlots = Object.assign({}, props.slots, inputSlots);
+            // 输入框
+            let inputItem = h(getInputItem(inputType), inputAttrs, inputSlots);
             // 输入事件
             function onUpdate(val) {
                 emit('update:modelValue', val, { label, ...attrs })
@@ -51,13 +49,7 @@ export default defineComponent({
             /***** 表单项组件 *****/
 
             // 表单项插槽
-            fieldSlots = {
-                ...fieldSlots, default: () => [
-                    beforeInput ? beforeInput() : null,
-                    inputItem,
-                    afterInput ? afterInput() : null
-                ]
-            }
+            fieldSlots.default = () => inputItem
 
             // 表单项属性
             const fieldAttrs = Object.assign({
